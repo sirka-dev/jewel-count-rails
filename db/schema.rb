62d5b85c48e7f53e08ba5ecef2cd3bd01bbb1da2
sirka-dev <45910564+sirka-dev@users.noreply.github.com>
2016-10-01 17:45:54 +0900

    イベントモデルのdb:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923180210) do
+ActiveRecord::Schema.define(version: 20161001084422) do
 
   create_table "counts", force: :cascade do |t|
     t.integer  "count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "start_date"
+    t.datetime "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
