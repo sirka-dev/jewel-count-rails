7c6c4284e70366269be9b48facec67e32a73d5b8
sirka-dev <45910564+sirka-dev@users.noreply.github.com>
2016-11-19 19:51:14 +0900

    Eventが二重に書かれていた誤記を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Event.Event.where(name: 'LIVE Groove Dance burst 201610').update(end_date: "2016/11/08 20:59:59")
+Event.where(name: 'LIVE Groove Dance burst 201610').update(end_date: "2016/11/08 20:59:59")
 Event.create(name:"Flip Flop", category:"アタポン形式", start_date: "2016/11/19 15:00:00", end_date: "2016/11/27 20:59:59")
