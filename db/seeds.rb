baef633583f35204adcebe959144240347edc0af
sirka-dev <45910564+sirka-dev@users.noreply.github.com>
2017-01-09 00:08:18 +0900

    イベントの追加用seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Event.create(name:"LIVE Parade 201611 (EVERMORE)", category:"Parade形式", start_date: "2016/11/30 15:00:00", end_date: "2016/12/08 20:59:59")
-Event.create(name:"あんきら！？狂騒曲", category:"アタポン形式", start_date: "2016/12/21 15:00:00", end_date: "2016/12/27 20:59:59")
+Event.create(name:"LIVE Groove Visual burst 201612", category:"Groove形式", start_date: "2016/12/31 15:00:00", end_date: "2017/01/09 20:59:59")
