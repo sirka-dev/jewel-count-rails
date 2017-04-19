8368be212b48e66f7f078b282d5dfd8d39db572f
sirka-dev <45910564+sirka-dev@users.noreply.github.com>
2017-04-20 01:36:28 +0900

    イベント追加の誤記修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,7 @@ class EventsController < ApplicationController
 
   def create
     if params["name"] != ""
-      Event.create( name: params["name"], category: params["category"], start_date: params["start_date"], end_date: params["end_date"])
+      Event.create( name: params["name"], category: params["category"], start_date: params["start_date"] + ' 15:00:00', end_date: params["end_date"] + ' 20:59:59')
       redirect_to :controller => :events, :action => :index, :notice => "登録しました"
     else
       redirect_to :controller => :events, :action => :index, :notice => "イベント名が空欄かも？"
