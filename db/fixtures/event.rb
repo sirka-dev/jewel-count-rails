3f9893586b2965ce871765ca4bf0c8db091e0ef2
sirka-dev <45910564+sirka-dev@users.noreply.github.com>
2017-10-07 14:51:46 +0900

    seed-fu用ファイルの追加（∀NSWERまで）

---
+++ db/fixtures/event.rb
@@ -0,0 +1,60 @@
+Event.seed(:name) do |s|
+  s.name = "ラブレター"
+  s.category = "アタポン形式"
+  s.start_date = "2016-09-20 15:00:00"
+  s.end_date = "2016-09-26 20:59:59"
+end
+Event.seed(:name) do |s|
+  s.name = "LIVE Parade 201609"
+  s.category = "Parade形式"
+  s.start_date = "2016-09-30 15:00:00"
+  s.end_date = "2016-10-08 20:59:59"
+end
+Event.seed(:name) do |s|
+  s.name = "Jet to the Future"
+  s.category = "アタポン形式"
+  s.start_date = "2016-10-20 15:00:00"
+  s.end_date = "2016-10-27 20:59:59"
+end
+Event.seed(:name) do |s|
+  s.name = "LIVE Groove Dance burst 201610"
+  s.category = "Groove形式"
+  s.start_date = "2016-10-31 15:00:00"
+  s.end_date = "2016-11-08 20:59:59"
+end
+Event.seed(:name) do |s|
+  s.name = "Flip Flop"
+  s.category = "アタポン形式"
+  s.start_date = "2016-11-19 15:00:00"
+  s.end_date = "2016-11-27 20:59:59"
+end
+Event.seed(:name) do |s|
+  s.name = "Lunatic Show"
+  s.category = "アタポン形式"
+  s.start_date = "2017/01/20 15:00:00"
+  s.end_date = "2017/01/27 20:59:59"
+end
+Event.seed(:name) do |s|
+  s.name = "LIVE Parade 201701"
+  s.category = "Parade形式"
+  s.start_date = "2017/01/31 15:00:00"
+  s.end_date = "2017/02/07 20:59:59"
+end
+Event.seed(:name) do |s|
+  s.name = "情熱ファンファンファーレ"
+  s.category = "アタポン形式"
+  s.start_date = "2017/02/17 15:00:00"
+  s.end_date = "2017/02/25 20:59:59"
+end
+Event.seed(:name) do |s|
+  s.name = "LIVE Groove Vocal burst 201702"
+  s.category = "Groove形式"
+  s.start_date = "2017/02/28 15:00:00"
+  s.end_date = "2017/03/09 20:59:59"
+end
+Event.seed(:name) do |s|
+  s.name = "∀NSWER"
+  s.category = "アタポン形式"
+  s.start_date = "2017/03/21 15:00:00"
+  s.end_date = "2017/03/28 20:59:59"
+end
